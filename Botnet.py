460910b346a7ae54863713c42af33f7786422d11
Cyb3rCl0wn <48457346+CYB3RCL0WN1@users.noreply.github.com>
2019-03-11 20:02:40 -0400

    Update Botnet.py

--- Botnet.py
+++ Botnet.py
@@ -13,12 +13,12 @@ def title_install():
 title_install()
 def code_bn():
 	os.system("toilet -F metal Hax4Us")
-	proxylist=(input(Fore.RED+"file destination for Tor Ip/Port"+Fore.RESET))
+	proxylist=(input(Fore.RED+"file destination for Tor Ip/Port : "+Fore.RESET))
 	
 	main_ip=(sk.gethostbyname(sk.gethostname()))
 	choose=(input(Fore.BLUE+"1.Start Tor"+" 2.DDOS Botnet"+" 0.Exit"+":   "+Fore.RESET))
 	if choose==("1"):
-		os.system("tor -f"+(proxylist))
+		os.system("tor -f "+(proxylist))
 	else:
 		print("Syntax Error ")
 code_bn()
