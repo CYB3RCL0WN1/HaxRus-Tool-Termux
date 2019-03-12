c1c1e8b436bee6883a55602669e2d21e32bf9807
Cyb3rCl0wn <48457346+CYB3RCL0WN1@users.noreply.github.com>
2019-03-12 18:05:24 -0400

    BOTNET PYTHON FILE

--- Botnet.py
+++ Botnet.py
@@ -8,17 +8,19 @@ def title_install():
 	os.system("apt install toilet && toilet Package Installed")
 	os.system("pip2 install colorama && toilet Module Installed")
 	os.system("apt install tor && figlet Tor Installed")
-	os.system("apt install figlet && apt install gay")
-	os.system("apt install openssl && apt install util-linux && apt install proxychains")
+	os.system("apt install figlet")
+	os.system("apt install openssl && apt install util-linux")
 title_install()
 def code_bn():
-	os.system("toilet -F metal Hax4Us")
-	proxylist=(input(Fore.RED+"file destination for Tor Ip/Port : "+Fore.RESET))
-	
+	os.system("toilet -F metal HAX4US")
 	main_ip=(sk.gethostbyname(sk.gethostname()))
-	choose=(input(Fore.BLUE+"1.Start Tor"+" 2.DDOS Botnet"+" 0.Exit"+":   "+Fore.RESET))
+	choose=(input(Fore.BLUE+"1.Start Tor"+" 2.DDOS Botnet "+"3.Edit Tor Config "+" 0.Exit"+":   "+Fore.RESET))
 	if choose==("1"):
-		os.system("tor -f "+(proxylist))
+		os.system("sudo cat proxychains.conf /etc/proxychains.conf")
+		os.system("sudo tor")
+	if choose==("3"):
+		os.system("nano /etc/proxychains.conf")
 	else:
 		print("Syntax Error ")
 code_bn()
+    
