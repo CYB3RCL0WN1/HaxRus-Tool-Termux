a8766d381f10a3a92313281fa4b0f8ed34db81d5
Cyb3rCl0wn <48457346+CYB3RCL0WN1@users.noreply.github.com>
2019-03-14 13:58:14 -0400

    Update Botnet.py

--- Botnet.py
+++ Botnet.py
@@ -2,23 +2,35 @@ import os
 import sys
 import socket as sk
 from colorama import Style,Fore,Back
+import pexpect
+import pxssh
 
-def title_install():
-	os.system("apt install openssh && pip2 install socket")
-	os.system("apt install toilet && toilet Package Installed")
-	os.system("pip2 install colorama && toilet Module Installed")
-	os.system("apt install tor && figlet Tor Installed")
-	os.system("apt install figlet && apt install gay")
-	os.system("apt install openssl && apt install util-linux && apt install proxychains")
-title_install()
+class DDOS():
+
 def code_bn():
-	os.system("toilet -F metal Hax4Us")
-	proxylist=(input(Fore.RED+"file destination for Tor Ip/Port"+Fore.RESET))
-	
-	main_ip=(sk.gethostbyname(sk.gethostname()))
-	choose=(input(Fore.BLUE+"1.Start Tor"+" 2.DDOS Botnet"+" 0.Exit"+":   "+Fore.RESET))
-	if choose==("1"):
-		os.system("tor -f"+(proxylist))
+	os.system("toilet -F metal HAX4US")
+	def _init_(self,host,user,password)
+	self.host=host
+		self.user=user
+		self.password=password
+		self.session()
+	main_1=(Fore.BLUE+"1.Start Tor")
+	main_2=("2.Start DDOS Botnet")
+	main_3=("3.Edit Proxychains/Tor file(requires root)")
+	main_4=("0.Exit Hax4Us"+Fore.RESET)
+	print (main_1)
+	print (main_2)
+	print (main_3)
+	print (main_4)
+	Choose=input(Fore.BLUE+"choose your option:"+Fore.RESET)
+	if Choose==("1"):
+		os.system("sudo tor")
+	if Choose==("3"):
+		os.system("nano /etc/proxychains.conf")
+	if Choose==("2")
+		os.system("python Main_bn.py")
 	else:
-		print("Syntax Error ")
+		print("Syntax Error...Please Try again later")
+	_init_()
 code_bn()
+DDOS()   
