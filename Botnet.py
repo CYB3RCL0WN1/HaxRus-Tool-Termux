7b111956d7c76e4b18bf1707eeac3d43f28d8e1a
Cyb3rCl0wn <48457346+CYB3RCL0WN1@users.noreply.github.com>
2019-03-14 17:00:56 -0400

    Add files via upload

--- Botnet.py
+++ Botnet.py
@@ -2,35 +2,30 @@ import os
 import sys
 import socket as sk
 from colorama import Style,Fore,Back
-import pexpect
-import pxssh
+
 
 class DDOS():
 
 	def code_bn():
+		main_1=(Fore.BLUE+"1.Start Tor")
+		main_2=("2.Start DDOS Botnet")
+		main_3=("3.Edit Proxychains/Tor file(requires root)")
+		main_4=("0.Exit Hax4Us"+Fore.RESET)
+		os.system("figlet 100% loaded")
 		os.system("toilet -F metal HAX4US")
-		def _init_(self,host,user,password)
-		self.host=host
-		self.user=user
-		self.password=password
-		self.session()
-	main_1=(Fore.BLUE+"1.Start Tor")
-	main_2=("2.Start DDOS Botnet")
-	main_3=("3.Edit Proxychains/Tor file(requires root)")
-	main_4=("0.Exit Hax4Us"+Fore.RESET)
-	print (main_1)
-	print (main_2)
-	print (main_3)
-	print (main_4)
-	Choose=input(Fore.BLUE+"choose your option:"+Fore.RESET)
-	if Choose==("1"):
-		os.system("sudo tor")
-	if Choose==("3"):
-		os.system("nano /etc/proxychains.conf")
-	if Choose==("2")
-		os.system("python Main_bn.py")
-	else:
-		print("Syntax Error...Please Try again later")
-	_init_()
-code_bn()
+		print (main_1)
+		print (main_2)
+		print (main_3)
+		print (main_4)
+		Choose=input(Fore.BLUE+"choose your option:"+Fore.RESET)
+		if Choose==("1"):
+			os.system("sudo tor")
+		if Choose==("3"):
+			os.system("nano /etc/proxychains.conf")
+		if Choose==("2"):
+			os.system("python Main_bn.py")
+		else:
+			print("Syntax Error...Please Try again later")
+	code_bn()
 DDOS()   
+    
